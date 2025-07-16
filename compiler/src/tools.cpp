979d277362618588454b3f7e53c2f541245f69fe
Ted Bäckman <jaaraargh@gmail.com>
2025-07-16 22:31:29 +0200

    Use snprintf due to risk of null termination instead of sprintf.

--- compiler/src/tools.cpp
+++ compiler/src/tools.cpp
@@ -406,12 +406,20 @@ std::string formatDate(time_t time)
 		return {};
 	}
 
-	char buffer[20];
-	int res = sprintf(buffer, "%02d/%02d/%04d %02d:%02d:%02d", tms->tm_mday, tms->tm_mon + 1, tms->tm_year + 1900, tms->tm_hour, tms->tm_min, tms->tm_sec);
-	if (res < 0) {
+	char buffer[32];
+	int res = snprintf(buffer, sizeof(buffer),
+	  "%02d/%02d/%04d %02d:%02d:%02d",
+	  tms->tm_mday,
+	  tms->tm_mon + 1,
+	  tms->tm_year + 1900,
+	  tms->tm_hour,
+	  tms->tm_min,
+	  tms->tm_sec);
+
+	if (res < 0 || res >= (int)sizeof(buffer)) {
 		return {};
 	}
-	return {buffer, 19};
+	return {buffer, 32};
 }
 
 std::string formatDateShort(time_t time)
